e46936290bfa216e4d0af2f6942873c6bad642fb
Gilber Moreno <6ilberM@gmail.com>
2020-11-15 08:47:07 -0500

    Monster Now Moves around randomly at a very fast interval

--- Assets/Scripts/Monster/MonsterAgentController.cs
+++ Assets/Scripts/Monster/MonsterAgentController.cs
@@ -9,36 +9,77 @@ public class MonsterAgentController : MonoBehaviour
     [SerializeField] private bool seenPlayer = false;
     [SerializeField] private SpriteRenderer monsterSprite;
     [SerializeField] private PlayerMovementController playerMovementController;
-    private Camera m_Camera = null;
 
-    private void Awake()
+    [SerializeField] private Camera m_Camera;
+
+    private void Awake() { m_Camera = Camera.main; }
+
+    private void Start()
     {
-        m_Camera = Camera.main;
+        StartCoroutine(GoToRandomPosition());
     }
 
-    private void Start() { StartCoroutine(LetsFollowBack()); }
-    private void Update()
+
+    private void OnTriggerEnter(Collider other)
     {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            seenPlayer = true;
+            StartCoroutine(LetsFollowBack());
+        }
     }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player")) { seenPlayer = false; }
+        StartCoroutine(GoToRandomPosition());
+    }
+
 
     private IEnumerator LetsFollowBack()
     {
-        while (true)
+        while (seenPlayer)
         {
             meshAgent.destination = GameObject.FindGameObjectWithTag("Player").transform.position;
 
             yield return new WaitForSeconds(Random.Range(0.15f, .45f));
         }
     }
+
+
+    private IEnumerator GoToRandomPosition()
+    {
+        while (!seenPlayer)
+        {
+            meshAgent.destination = RandomNavmeshLocation(26);
+
+            yield return new WaitForSeconds(Random.Range(0.4f, 1f));
+        }
+    }
+
     void LateUpdate()
     {
         transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.forward,
             m_Camera.transform.rotation * Vector3.up);
     }
+
     private void OnValidate()
     {
         if (meshAgent == null) { meshAgent = GetComponent<NavMeshAgent>(); }
         if (monsterSprite == null) { monsterSprite = GetComponentInChildren<SpriteRenderer>(); }
+    }
+
+    public Vector3 RandomNavmeshLocation(float radius)
+    {
+        Vector3 randomDirection = Random.insideUnitSphere * radius;
+        randomDirection += transform.position;
+        NavMeshHit hit;
+        Vector3 finalPosition = Vector3.zero;
+        if (NavMesh.SamplePosition(randomDirection, out hit, radius, 1))
+        {
+            finalPosition = hit.position;
+        }
+        return finalPosition;
     }
 
 }
